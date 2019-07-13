8be7f02b41892bfa2228354cf99e3cd956fc7f76
joseluiseiguren <joseluiseiguren@gmail.com>
2019-07-13 11:34:41 +0200

    modify connection to mongo

--- src/mongo.repositories/conection.ts
+++ src/mongo.repositories/conection.ts
@@ -2,8 +2,16 @@ var config = require('../../src/config');
 var mongoose = require('mongoose');
 
 // se abre la conexion con la base de datos mongo
-mongoose.connect(config.db.strconexion, function(error) {
+/*mongoose.connect(config.db.strconexion, function(error) {
     if(error !== null){
         throw ({message: "No se Pudo conectar a la base de datos"});
     }
-}); 
+});*/
+
+
+var options =  { keepAlive: 1, connectTimeoutMS: 30000, reconnectTries: 30, reconnectInterval: 5000 }
+mongoose.connect(config.db.strconexion, options, (err) => {
+    if(err) {
+        console.error("Error while connecting", err);
+    }
+});
